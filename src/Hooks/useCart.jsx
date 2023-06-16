d9b764a4b5f66b8a11432bad289bf7941afd2b8f
najmul19977 <najmulkabir19977@gmail.com>
2023-06-16 21:36:15 +0600

    jwt commit

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,15 +1,26 @@
 import { useContext } from 'react';
 import { useQuery } from 'react-query'
 import { AuthContext } from '../Provider/AuthProvider';
+import useAxiosSecure from './useAxiousSecure';
 
 const useCart = () =>{
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data :cart= []} = useQuery({
+    /*     queryKey:['carts',user?.email],
+        queryFn: async () =>{
+            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`,{headers:{
+                authorization:`bearer ${token }`
+            }})
+            return response.json();
+        }, */
         queryKey:['carts',user?.email],
         queryFn: async () =>{
-            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`)
-            return response.json();
+            const res = await axiosSecure(`/carts?email=${user.email}`)
+            console.log('res from axios',res)
+            return res.data;
         },
 
     })
